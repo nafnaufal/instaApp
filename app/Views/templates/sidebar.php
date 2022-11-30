5d43f1670704bcf63bc857d01e1356e54d90f058
nafnaufal <naufalanbial@gmail.com>
2022-11-30 15:01:22 +0700

    font for logo

--- app/Views/templates/sidebar.php
+++ app/Views/templates/sidebar.php
@@ -9,6 +9,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oleo+Script&display=swap" rel="stylesheet">
     <title>Timeline</title>
 </head>
 
@@ -17,9 +20,9 @@
     <div class="container-sm">
         <nav class="py-2 px-5 navbar fixed-bottom navbar-light bg-white">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">instaApp</a>
+                <a class="navbar-brand" href="#" style='font-family: "Oleo Script", cursive;'>instaApp</a>
                 <a href="#">
-                    <i class="bi bi-plus-square fs-1"></i>
+                    <i class="bi bi-plus-square text-dark fs-1"></i>
                 </a>
                 <a href="#">
                     <i class="bi bi-power text-danger fs-3"></i>
